ae5d89eb267efc59b3bf6a95ba9e3813b514775a
Marko Panchak <marko.panchak1@gmail.com>
2024-08-30 20:35:50 +0300

    Solution

--- main.tf
+++ main.tf
@@ -1,25 +1,29 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.105.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  depends_on               = [azurerm_resource_group.example]
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = var.container_name
+  storage_account_name  = var.storage_account_name
+  container_access_type = "private"
+  depends_on            = [azurerm_storage_account.example]
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = var.blob_name
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.container_name
+  type                   = "Block"
+  source                 = "terraform_code.zip"
+  depends_on             = [azurerm_storage_container.example]
 }
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type    = string
+  default = "example-resources"
+}
+
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = "panchakstorage1"
+}
+
+variable "container_name" {
+  type    = string
+  default = "vhds"
+}
+
+variable "blob_name" {
+  type    = string
+  default = "example-blob-name"
+}
